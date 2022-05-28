bebb019ae45180aa8205a27a4e8a358fffc903b2
avinashthange007 <avinashthange007@gmail.com>
2022-05-28 17:27:17 +0530

    By-Mr.Avinash1

---
+++ src/test/java/cucumbermap/Demo1.java
@@ -0,0 +1,11 @@
+package cucumbermap;
+
+public class Demo1 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Demo method gets called");
+		System.out.println("Demo method gets called");
+	}
+	
+}
