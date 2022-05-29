ced76116f2332d5bdcf8c4d71f00951ad28cce6f
avinashthange007 <avinashthange007@gmail.com>
2022-05-29 16:56:59 +0530

    By David on Develop

---
+++ src/test/java/cucumbermap/DavidWork1.java
@@ -0,0 +1,12 @@
+package cucumbermap;
+
+public class DavidWork1 
+{
+
+		public static void main(String[]args)
+		{
+			System.out.println("David Work Done");
+			System.out.println("David Work Done");
+		}
+		
+}
